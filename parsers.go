8c4990ca8f4077d0bd2b31ce241d0b0d294bcc89
Mike <18002494+mikeunge@users.noreply.github.com>
2024-06-13 08:37:43 +0200

    fix: assign result

--- parsers.go
+++ parsers.go
@@ -7,10 +7,12 @@ func (p *Parser) parseString(cmd Command, args []string) error {
 	for i := 0; i < len(args); i++ {
 		if args[i] == cmd.long || args[i] == cmd.short {
 			if i+1 >= len(args) {
-				return fmt.Errorf("String parser expects a value after the flag, nothing found.")
+				return fmt.Errorf("string parser expects a value after the flag, nothing found.")
 			}
-			fmt.Println(args[i+1])
+			*cmd.found = true
+			*cmd.result.(*string) = args[i+1]
+			return nil
 		}
 	}
-	return nil
+	return fmt.Errorf("nothing to parse.")
 }
