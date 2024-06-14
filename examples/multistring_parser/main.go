051e42f3621e1d00681e58b02cb3b01d0267e052
Mike <18002494+mikeunge@users.noreply.github.com>
2024-06-14 21:40:10 +0200

    feat: add examples

---
+++ examples/multistring_parser/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mikeunge/argparser"
+)
+
+const (
+	APP_NAME = "MultiStringParser"
+	APP_DESC = "Multi string parser example."
+)
+
+func main() {
+	parser := argparser.NewParser(APP_NAME, APP_DESC)
+	result, found := parser.MultiString("-l", "--list", &argparser.Options{Required: false, Help: "Echo's whatever you provide."})
+
+	if err := parser.Parse(); err != nil {
+		fmt.Printf("Parser returned with error: %s\n", err.Error())
+		parser.PrintHelp()
+		os.Exit(1)
+	}
+
+	if !*found {
+		fmt.Println("No argument provided for --list, goodbye!")
+		os.Exit(0)
+	}
+
+	for _, res := range *result {
+		fmt.Println(res)
+	}
+}
---
+++ examples/string_parser/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mikeunge/argparser"
+)
+
+const (
+	APP_NAME = "StringParser"
+	APP_DESC = "String parser example."
+)
+
+func main() {
+	parser := argparser.NewParser(APP_NAME, APP_DESC)
+	result, found := parser.String("-e", "--echo", &argparser.Options{Required: false, Help: "Echo's whatever you provide."})
+
+	if err := parser.Parse(); err != nil {
+		fmt.Printf("Parser returned with error: %s\n", err.Error())
+		parser.PrintHelp()
+		os.Exit(1)
+	}
+
+	if !*found {
+		fmt.Println("No argument provided for --echo, goodbye!")
+		os.Exit(0)
+	}
+	fmt.Printf("%s\n", *result)
+}
