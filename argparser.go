53acf9bf55270350263e0aa776378afb3e730ed6
Mike <18002494+mikeunge@users.noreply.github.com>
2024-06-15 19:58:16 +0200

    feat: add command factory, cleanup code, finish auto generated --help

--- argparser.go
+++ argparser.go
@@ -3,6 +3,7 @@ package argparser
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 type CommandType int
@@ -56,13 +57,54 @@ func (p *Parser) GetArgc() int {
 	return p.argc
 }
 
-func (p *Parser) PrintHelp() {
+func (p *Parser) Usage(info string) {
 	if !p.printHelp {
 		return
 	}
-	fmt.Printf("%s - %s\n\nAvailable arguments:\n", p.name, p.description)
+
+	maxShort := 0
+	maxLong := 0
+	for _, c := range *p.commands {
+		if len(c.short) > maxShort {
+			maxShort = len(c.short)
+		}
+		if len(c.long) > maxLong {
+			maxLong = len(c.long)
+		}
+	}
+
+	formatHelpTable := func(cmd Command) string {
+		// decide what option to display
+		opt := ""
+		if len(cmd.short) > 0 && len(cmd.short) > 0 {
+			opt = fmt.Sprintf("%s %s", cmd.short, cmd.long)
+		} else if len(cmd.short) > 0 {
+			opt = fmt.Sprintf("%s", cmd.short)
+		} else {
+			space := strings.Repeat(" ", maxShort)
+			opt = fmt.Sprintf("%s %s", space, cmd.long)
+		}
+
+		// make sure the option(s) always have the same lenght so it doesn't look so scuffed
+		if len(opt) < (maxShort + maxLong + 1) {
+			diff := maxShort + maxLong + 1 - len(opt)
+			space := strings.Repeat(" ", diff)
+			opt = fmt.Sprintf("%s%s", opt, space)
+		}
+
+		if cmd.opts.Required {
+			return fmt.Sprintf("\t%s\t%s (required)", opt, cmd.opts.Help)
+		}
+		return fmt.Sprintf("\t%s\t%s", opt, cmd.opts.Help)
+	}
+
+	if len(info) > 0 {
+		fmt.Println(info)
+	}
+	fmt.Printf("%s - %s\n\nAvailable options:\n", p.name, p.description)
 	for _, cmd := range *p.commands {
-		fmt.Printf("\t%s\t%s required: %t\n", fmt.Sprintf("%s, %s", cmd.short, cmd.long), cmd.opts.Help, cmd.opts.Required)
+		availableOption := formatHelpTable(cmd)
+		fmt.Println(availableOption)
 	}
 }
 
@@ -71,8 +113,16 @@ func (p *Parser) Parse() error {
 	p.argc = len(args) - 1 // remove one (1) because the first arg is always the app name
 
 	if p.GetArgc() == 0 {
-		p.PrintHelp()
+		p.Usage("No arguments provided.")
 		return nil
+	}
+
+	// check if the user provided --help as flag
+	for _, arg := range args {
+		if arg == "--help" {
+			p.Usage("")
+			return nil
+		}
 	}
 
 	for _, cmd := range *p.commands {
@@ -111,17 +161,8 @@ func (p *Parser) String(short string, long string, opts *Options) (*string, *boo
 		found  bool
 	)
 
-	// TODO: verify opts
-
-	cmd := Command{
-		result:      &result,
-		found:       &found,
-		commandType: StringParser,
-		short:       short,
-		long:        long,
-		opts:        *opts,
-	}
-	*p.commands = append(*p.commands, cmd)
+	command := makeCommand(short, long, &result, &found, StringParser, opts)
+	*p.commands = append(*p.commands, command)
 
 	return &result, &found
 }
@@ -130,15 +171,8 @@ func (p *Parser) MultiString(short string, long string, opts *Options) (*[]strin
 	var found bool
 
 	result := make([]string, 0)
-	cmd := Command{
-		result:      &result,
-		found:       &found,
-		commandType: MultiStringParser,
-		short:       short,
-		long:        long,
-		opts:        *opts,
-	}
-	*p.commands = append(*p.commands, cmd)
+	command := makeCommand(short, long, &result, &found, MultiStringParser, opts)
+	*p.commands = append(*p.commands, command)
 
 	return &result, &found
 }
@@ -149,15 +183,8 @@ func (p *Parser) Flag(short string, long string, opts *Options) (*bool, *bool) {
 		found  bool
 	)
 
-	cmd := Command{
-		result:      &result,
-		found:       &found,
-		commandType: FlagParser,
-		short:       short,
-		long:        long,
-		opts:        *opts,
-	}
-	*p.commands = append(*p.commands, cmd)
+	command := makeCommand(short, long, &result, &found, FlagParser, opts)
+	*p.commands = append(*p.commands, command)
 
 	return &result, &found
 }
@@ -168,36 +195,31 @@ func (p *Parser) Number(short string, long string, opts *Options) (*int, *bool) 
 		found  bool
 	)
 
-	cmd := Command{
-		result:      &result,
-		found:       &found,
-		commandType: NumberParser,
-		short:       short,
-		long:        long,
-		opts:        *opts,
-	}
-	*p.commands = append(*p.commands, cmd)
+	command := makeCommand(short, long, &result, &found, NumberParser, opts)
+	*p.commands = append(*p.commands, command)
 
 	return &result, &found
 }
 
 func (p *Parser) MultiNumber(short string, long string, opts *Options) (*[]int, *bool) {
-	var (
-		result []int
-		found  bool
-	)
+	var found bool
+	result := make([]int, 0)
 
-	cmd := Command{
-		result:      &result,
-		found:       &found,
-		commandType: MultiNumberParser,
+	command := makeCommand(short, long, &result, &found, MultiNumberParser, opts)
+	*p.commands = append(*p.commands, command)
+
+	return &result, &found
+}
+
+func makeCommand(short string, long string, result interface{}, found *bool, cmdType CommandType, opts *Options) Command {
+	return Command{
+		result:      result,
+		found:       found,
+		commandType: cmdType,
 		short:       short,
 		long:        long,
 		opts:        *opts,
 	}
-	*p.commands = append(*p.commands, cmd)
-
-	return &result, &found
 }
 
 func determineUsedFlag(c *Command) string {
